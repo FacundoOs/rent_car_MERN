d7fec15874476991ac4a41adff15a18775b09c60
facundo.osores <facundo.osores@healthintegrator.eu>
2021-08-27 21:43:49 +0200

    set up TypeOrm with MySQL database on Netsjs

---
+++ nestjs-rental-car/ormconfig.js
@@ -0,0 +1,15 @@
+module.exports = {
+  name: 'dev',
+  type: 'mysql',
+  host: 'localhost',
+  port: 3306,
+  username: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME,
+  entities: ['dist/entities/*{.ts,.js}'],
+  synchronize: true,
+  migrations: ['dist/migrations/*{.ts,.js}'],
+  cli: {
+    migrationsDir: 'src/migrations',
+  },
+};
