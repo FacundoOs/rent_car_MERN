701223ec67c85233d6d22c4551f4cf18ac66eae5
facundo.osores <facundo.osores@healthintegrator.eu>
2021-08-31 17:10:39 +0200

    create car entity, fetching cars from database and create add new car mutation

--- nestjs-rental-car/ormconfig.js
+++ nestjs-rental-car/ormconfig.js
@@ -6,7 +6,7 @@ module.exports = {
   username: process.env.DB_USER,
   password: process.env.DB_PASS,
   database: process.env.DB_NAME,
-  entities: ['dist/entities/*{.ts,.js}'],
+  entities: ['dist/**/entities/*{.ts,.js}'],
   synchronize: true,
   migrations: ['dist/migrations/*{.ts,.js}'],
   cli: {
